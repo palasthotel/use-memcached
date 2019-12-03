6a53823b4345c5a3942f962b731390615a239f3f
EdwardBock <edward.bock@palasthotel.de>
2019-12-03 18:39:24 +0100

    js api

--- admin.js
+++ admin.js
@@ -1,14 +1,50 @@
-jQuery(function($){
-	console.log("use-memcached admin.js");
+
+(function($, Settings){
+
+	console.log(Settings);
+
+	$(function(){
+		addEventHandlers();
+	});
+
+	function addEventHandlers(){
+		$("#wp-admin-bar-use-memcached-flush").click(ajax_flush_request);
+	}
+
+	/**
+	 * flush memcached
+	 */
 	let isFlushing = false;
-	$("#wp-admin-bar-use-memcached-flush").click(function(){
-		console.log("clicked");
+	function ajax_flush_request(){
 		if(isFlushing) return;
 		isFlushing = true;
-		console.log("flushing");
-		$.post("/wp-admin/admin-ajax.php",{action:"use_memcached_flush"}, function(res){
-			isFlushing = false;
-			console.log(res);
-		});
-	});
-});
+		$.post(Settings.ajaxUrl,{action: Settings.actions.flush}, ajax_flush_response);
+	}
+	function ajax_flush_response(response){
+		isFlushing = false;
+		console.log(response);
+	}
+
+	/**
+	 * memcached stats
+	 */
+	let isRequestingStats = false;
+	function ajax_stats_request(){
+		if(isRequestingStats) return;
+		isRequestingStats =  true;
+		$.post(Settings.ajaxUrl, {action: Settings.actions.stats}, ajax_stats_response);
+	}
+	function ajax_stats_response(response){
+		isRequestingStats = false;
+		console.log(response);
+	}
+
+	// ------------------------------
+	// expose api to public
+	// ------------------------------
+	Settings.api = {
+		flush: ajax_flush_request,
+		stats: ajax_stats_request,
+	};
+
+})(jQuery, UseMemcached);
